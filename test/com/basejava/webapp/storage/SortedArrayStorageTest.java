f8953902189ba02f4b4e65f3d0ae1bb605db9eba
AlexeyPavlov2 <alexey.pavlov2@gmail.com>
2018-06-15 16:41:37 +0300

    Improved tests for Storage classes

--- test/com/basejava/webapp/storage/SortedArrayStorageTest.java
+++ test/com/basejava/webapp/storage/SortedArrayStorageTest.java
@@ -1,7 +1,21 @@
 package com.basejava.webapp.storage;
 
+import com.basejava.webapp.model.Resume;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+    private static final String UUID_00 = "uuid00";
+    private static final Resume RESUME00 = new Resume(UUID_00);
+
     public SortedArrayStorageTest() {
         super(new SortedArrayStorage());
+    }
+
+    @Test
+    public void checkIsSorted() {
+        storage.save(RESUME00);
+        Resume[] actual = storage.getAll();
+        assertEquals(RESUME00, actual[0]);
     }
 }
