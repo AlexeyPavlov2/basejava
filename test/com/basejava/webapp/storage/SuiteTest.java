b96a492959a843b2804ba6f579e040f722d98078
AlexeyPavlov2 <alexey.pavlov2@gmail.com>
2018-09-16 01:07:51 +0300

    Renamed classes AbstractFile.PathStorage. Implemented strategy pattern using FileSystemDriver

--- test/com/basejava/webapp/storage/SuiteTest.java
+++ test/com/basejava/webapp/storage/SuiteTest.java
@@ -10,7 +10,7 @@ import org.junit.runners.Suite;
         ListStorageTest.class,
         MapUuidStorageTest.class,
         MapStorageTest.class,
-        ObjectStreamStorageTest.class,
+        ObjectStreamFileStorageTest.class,
         ObjectStreamPathStorageTest.class
     }
 )
