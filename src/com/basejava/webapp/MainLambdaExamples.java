16a37de951dcf6af04bfa5006450e71a9cd711a6
AlexeyPavlov2 <alexey.pavlov2@gmail.com>
2018-10-07 23:41:52 +0300

    Fixed bugs and implemented oddOrEven method

--- src/com/basejava/webapp/MainLambdaExamples.java
+++ src/com/basejava/webapp/MainLambdaExamples.java
@@ -1,59 +1,54 @@
 package com.basejava.webapp;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
 
 public class MainLambdaExamples {
     public static void main(String[] args) {
         //HW12 - i.
-        int count = 10;
-        int[] values =  new Random().ints(1,9)
+        int count = 6;
+        int[] values = new Random().ints(1, 9)
                 .limit(count).toArray();
-        System.out.print("Original int array: {");
-        for (int el : values) {
-            System.out.print(el + " ");
-        }
-        System.out.println("}");
-        System.out.print("minValues: ");
-        System.out.println(minValue(values) + "\n");
+        System.out.println(stream(values).boxed().map(String::valueOf)
+                .collect(Collectors.joining(", ", "Original int array: {", "}")));
+        System.out.print("minValues: " + minValue(values) + "\n");
 
         //HW12 - ii.
         int[] array = new Random().ints(1, 300).limit(count).toArray();
-        List<Integer> list = new ArrayList<>();
-        System.out.print("Original int list: {");
-        int sum = 0;
-        for (Integer el : array) {
-            list.add(el);
-            sum += el;
-            System.out.print(el + " ");
-        }
-        System.out.print("}");
-        System.out.println(" , sum of the elements " + sum + " is "  + (sum % 2 == 0 ? "even" : "odd"));
-        System.out.print("Modified int list: {");
-        list = oddOrEven(list);
-        for (int el : list) {
-            System.out.print(el + " ");
-        }
-        System.out.println("}");
-
+        List<Integer> list = Arrays.stream(array).boxed().collect(toList());
+        int sum = list.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(list.stream().map(String::valueOf)
+                .collect(Collectors.joining(", ", "Original int list: {", "} , sum of the elements " + sum + " is " + (sum % 2 == 0 ? "even" : "odd"))));
+        System.out.println(oddOrEven(list).stream().map(String::valueOf)
+                .collect(Collectors.joining(", ", "Modified int list: {", "}")));
     }
 
     private static int minValue(int[] values) {
-        Set<Integer> set = Arrays.stream(values)
-                .distinct().boxed().sorted().collect(Collectors.toCollection(LinkedHashSet::new));
-        int count = set.size(), value = 0;
-        for(Integer el : set) {
-            value += el * ((Double) Math.pow(10, --count)).intValue();
-        }
-        return value;
+        return stream(values)
+                .distinct()
+                .sorted()
+                .reduce((a, b) -> b = b + a * 10).getAsInt();
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        boolean flag = integers.stream().mapToInt(i -> i).sum() % 2 == 0;
-        return integers.stream()
-                .filter(el -> flag == (el % 2 != 0))
-                .collect(Collectors.toList());
+        int[] sum = {0};
+        Predicate<Integer> isEven = i -> i % 2 == 0;
+        return integers.stream().filter(el -> {
+            sum[0] = sum[0] + el;
+            return true;
+        }).collect(toList()).stream()
+                .filter(el -> {
+                    if (isEven.test(sum[0])) {
+                        return !isEven.test(el);
+                    } else return isEven.test(el);
+                })
+                .collect(toList());
     }
-
 
 }
